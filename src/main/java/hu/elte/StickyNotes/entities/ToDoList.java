9f31b04e1c5855f18c6021cce13bc0a3ee95733f
salgokrisz <salgokrisz@gmail.com>
2018-10-11 14:56:48 +0200

    dunno what im doing...

--- src/main/java/hu/elte/StickyNotes/entities/ToDoList.java
+++ src/main/java/hu/elte/StickyNotes/entities/ToDoList.java
@@ -17,5 +17,47 @@ public class ToDoList {
 
     @Id
     private Integer id;
+
+    @Column(nullable = false)
+	@NotNull
+	private String author;
+	
+	@Column(nullable = false)
+	@NotNull
+	private String name;
     
+	@Column(nullable = false)
+	@NotNull
+	private String text;
+	
+	
+	@Column(nullable = false)
+	@NotNull
+	private boolean complete = false;
+	
+	public ToDoList(){}
+	
+	public ToDoList(String author, String name, String text){
+		this.author = author;
+		this.name = name;
+		this.text = text;
+	}
+	
+	
+	public String getAuthor(){
+		return this.author;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public boolean isComplete(){
+		return this.complete;
+	}
+	
+	public void setComplete(boolean complete){
+		this.complete = complete;
+	}
+	
 }
