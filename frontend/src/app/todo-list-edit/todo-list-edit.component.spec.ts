2a372f73d3f36b0ff9bea8d1feb08ea5c3ca48dc
Xyrusson <hajdumark12@freemail.hu>
2018-12-11 20:39:47 +0100

    Create todo-list-edit.component.spec.ts

---
+++ frontend/src/app/todo-list-edit/todo-list-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoListEditComponent } from './todo-list-edit.component';
+
+describe('TodoListEditComponent', () => {
+  let component: TodoListEditComponent;
+  let fixture: ComponentFixture<TodoListEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoListEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoListEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
